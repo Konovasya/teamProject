c7700eafb7b99b557318e4349faa78a41a4fbc3d
Студент НИУ ВШЭ <student@SHAB-3118-STD13.staff.corp.local>
2019-12-14 17:53:41 +0300

    Добавьте файлы проекта.

---
+++ WpfApp/Question.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp
+{
+    class Question
+    {
+        public int Number { get; set; }
+
+        public string Text { get; set; }
+
+        public string FirstAnswer { get; set; }
+
+        public string SecondAnswer { get; set; }
+
+        public string ThirdAnswer { get; set; }
+
+        public int CorrectAnswer { get; set; }
+    }
+}
